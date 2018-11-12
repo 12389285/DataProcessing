595dd4240af2bd6e93b308b1a788ab854548fd89
Max Simons <max_simons@live.nl>
2018-11-12 13:49:15 +0100

    central tendency

--- Homework/week_2/eda.py
+++ Homework/week_2/eda.py
@@ -3,11 +3,11 @@
 import csv
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 INPUT_FILE = "input.csv"
 
-def file(INPUT_FILE):
-    #
+def main(INPUT_FILE):
     with open(INPUT_FILE) as file:
         input_reader = csv.DictReader(file)
 
@@ -17,6 +17,7 @@ def file(INPUT_FILE):
         population_density = []
         infant_mortality = []
         gdp_dollars = []
+        gdp_freq = []
 
         # make dictionary for data storage
         data_dict = {str(key): [] for key in range(len(countries))}
@@ -25,16 +26,27 @@ def file(INPUT_FILE):
             # get data from csv file per row
             country = row['Country'].strip()
             region = row['Region'].strip()
-            pop_den = row['Pop. Density (per sq. mi.)']
-            infant = row['Infant mortality (per 1000 births)']
+            pop_den = row['Pop. Density (per sq. mi.)'].strip().replace(',', '.')
+            infant = row['Infant mortality (per 1000 births)'].strip().replace(',', '.')
             gdp = row['GDP ($ per capita) dollars'].strip(' dollars')
-            if infant == '':
-                infant = 'unknown'
-            if gdp == 'unknown' or gdp == '':
+
+            # missing or invalid values
+            if pop_den == 'unknown' or pop_den == '':
+                pop_den = None
+            if infant == 'unknown' or infant == '':
+                infant = None
+            if gdp == 'unknown' or gdp == '' or gdp == '400000':
                 gdp = None
+
+            # make floats
             if gdp != None:
                 gdp = float(gdp)
-            # print(gdp)
+                # make list without None values for histogram
+                gdp_freq.append(int(gdp))
+            if infant != None:
+                infant = float(infant)
+            if pop_den != None:
+                pop_den = float(pop_den)
 
             # add data to all lists
             countries.append(country)
@@ -43,26 +55,33 @@ def file(INPUT_FILE):
             infant_mortality.append(infant)
             gdp_dollars.append(gdp)
 
-        # map(float, gdp_dollars)
-        # print(gdp_dollars)
 
         # add data to data_dict
         data_dict['Country'] = countries
         data_dict['Region'] = regions
-        data_dict['Pop. Density'] = population_density
-        data_dict['Infant mortality'] = infant_mortality
+        data_dict['Pop_Density'] = population_density
+        data_dict['Infant_mortality'] = infant_mortality
         data_dict['GDP_dollars'] = gdp_dollars
 
+        # make dataframe with all data
         df = pd.DataFrame(data_dict)
+
+        # get the mean, median, mode and standard deviation from GDP data
         mean = round(df.GDP_dollars.mean(), 2)
         median = int(df.GDP_dollars.median())
         mode = int(df.GDP_dollars.mode())
+        deviation  = round(df.GDP_dollars.std(), 2)
 
-        print(mean, median, mode)
+        # plot GDP data using histogram with the frequency of GDP_dollars
+        # see analyze.txt for explanation
+        plt.hist(gdp_freq, bins=25, histtype='bar', rwidth=0.8, color='g')
+        plt.xlabel('GDP in dollars')
+        plt.ylabel('Frequency')
+        plt.title('Frequency of GDP')
 
-
+        plt.show()
         # print(df)
 
 
 if __name__ == "__main__":
-    file(INPUT_FILE)
+    main(INPUT_FILE)
