2048638a82b7db09c51212004ee8322ff305c432
Max Simons <max_simons@live.nl>
2018-11-19 17:39:28 +0100

    v2

--- Homework/week_3/convertCSV2JSON.py
+++ Homework/week_3/convertCSV2JSON.py
@@ -7,19 +7,15 @@ import json
 import pandas as pd
 
 # fill in input file name .csv
-INPUT_FILE = "2018.csv"
+INPUT_FILE = "resultaten.csv"
 
 # fill in output file name .json
-OUTPUT_FILE = "2018.json"
-
-# fill in the fieldnames
-fieldnames = ("GRAND PRIX" , "DATE", "CAR", "RACE POSITION", "PTS")
+OUTPUT_FILE = "resultaten.json"
 
 def convert(INPUT_FILE):
 
     # read csv file with panda
     df = pd.read_csv(INPUT_FILE)
-
     # make json file based on records
     df.reset_index().to_json(OUTPUT_FILE, orient='records')
 
